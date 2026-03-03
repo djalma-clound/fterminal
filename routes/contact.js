43e92596133b723a590dcdee74f5cec75241323f
djalma-clound <djalmad841@gmail.com>
2026-03-02 23:02:07 -0500

    email

--- routes/contact.js
+++ routes/contact.js
@@ -13,14 +13,14 @@ router.post('/', async (req, res) => {
     );
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
